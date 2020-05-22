8d9287ddca0bbdaf128e46d6e37e5347aa1a3322
Laura Enria <enria.laura2@gmail.com>
2020-05-22 15:50:14 +0100

    add 4561_Traffic_Lights_2

--- src/main/scala/exercises/chapter4/J_4561_Traffic_Lights_2.scala
+++ src/main/scala/exercises/chapter4/J_4561_Traffic_Lights_2.scala
@@ -8,17 +8,36 @@ class J_4561_Traffic_Lights_2 {
 */
 }
 
-trait TrafficLight
+sealed trait Rainbow
+case class Pink() extends Rainbow
+case object Red extends Rainbow
+case object Orange extends Rainbow
+case object Yellow extends Rainbow
+case object Green  extends Rainbow
+case object Blue  extends Rainbow
+case object Indigo  extends Rainbow
 
-case object Red extends TrafficLight
-case object Green extends TrafficLight
-case object Yellow extends TrafficLight
 
-object test {
-  def next: TrafficLight =
-    this match {
-      case Red => Green
-      case Green => Yellow
-      case Yellow => Red
+sealed trait LivigBeing
+case class Crab() extends LivigBeing
+case class Tiger() extends LivigBeing
+case class Bee() extends LivigBeing
+case class Frog() extends LivigBeing
+case class Whale() extends LivigBeing
+case class Birds() extends LivigBeing
+case class Pig() extends LivigBeing
+
+
+
+object Body {
+  def color(living : LivigBeing):Rainbow =
+    living match {
+      case Crab() => Red
+      case Tiger() => Orange
+      case Frog() => Green
+      case Whale() => Blue
+      case Birds() => Indigo
+      case Pig() => Pink()
+
     }
 }
--- src/main/scala/exercises/chapter4/K_4562_Calculation.scala
+++ src/main/scala/exercises/chapter4/K_4562_Calculation.scala
@@ -1,5 +1,12 @@
 package exercises.chapter4
 
-class K_4562_Calculation {
+class K_4562_Calculation()
 
+sealed trait Calculation
+final case class Success(result: Int) extends Calculation
+final case class Failure(reason: String) extends Calculation
+
+object Calculator {
+  def +(c:Calculation, n:Int): Calculation = ???
+  def -(c:Calculation, n:Int): Calculation = ???
 }
