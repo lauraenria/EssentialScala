0c7d265f2aa75479d873098119d85a3b41bd60b1
Laura Enria <enria.laura2@gmail.com>
2020-05-22 15:50:14 +0100

    add: 4561 Traffic Lights2 and 4562 Calculation

---
+++ src/test/scala/exercises/chapter4/K_4562_CalculationTest.scala
@@ -0,0 +1,10 @@
+package exercises.chapter4
+import org.scalatest.FunSuite
+
+class K_4562_CalculationTest extends FunSuite {
+  test("Calculator test") {
+    assert(Calculator.+(Success(1), 1) == Success(2))
+    assert(Calculator.-(Success(1), 1) == Success(0))
+    assert(Calculator.+(Failure("Badness"), 1) == Failure("Badness"))
+  }
+}
