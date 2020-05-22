655cd3c2ce809ace52ad78952b3b289f4a71baf2
Laura Enria <enria.laura2@gmail.com>
2020-05-22 18:47:09 +0100

    add 4563 Email

--- src/main/scala/exercises/chapter4/K_4562_Calculation.scala
+++ src/main/scala/exercises/chapter4/K_4562_Calculation.scala
@@ -21,8 +21,22 @@ object Calculator extends App {
     }
   }
 
+  def /(c:Calculation, n:Int): Calculation  = {
+    c match {
+      case Success(result) if n != 0 => Success(result / n )
+      case Success(result) if n == 0 => Failure("Division by zero")
+      case Failure(reason) => Failure(reason)
+    }
+  }
+
+
   assert(Calculator.+(Success(1), 1) == Success(2))
   assert(Calculator.+(Success(1), 2) == Success(3))
   assert(Calculator.-(Success(1), 1) == Success(0))
   assert(Calculator.+(Failure("Badness"), 1) == Failure("Badness"))
+
+  //  second part
+  assert(Calculator./(Success(4), 2) == Success(2))
+  assert(Calculator./(Success(4), 0) == Failure("Division by zero"))
+  assert(Calculator./(Failure("Badness"), 0) == Failure("Badness"))
 }
--- src/main/scala/exercises/chapter4/M_4631_A_List_of_Methods.scala
+++ src/main/scala/exercises/chapter4/M_4631_A_List_of_Methods.scala
@@ -1,5 +1,7 @@
 package exercises.chapter4
 
-class M_4631_A_List_of_Methods {
+class M_4631_A_List_of_Methods
 
-}
+sealed trait IntList
+case object End extends IntList
+final case class Pair(head: Int, tail: IntList) extends IntList
