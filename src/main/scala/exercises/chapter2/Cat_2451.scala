d89875a6e7606277289d9dbd95e69cc3442b4351
Laura Enria <enria.laura2@gmail.com>
2020-08-13 16:13:24 +0100

    Chapter5 add:exercise 2452 2453 5131 5132-part2

--- src/main/scala/exercises/chapter2/Cat_2451.scala
+++ src/main/scala/exercises/chapter2/Cat_2451.scala
@@ -1,5 +1,35 @@
 package exercises.chapter2
 
 class Cat_2451 {
+  /*
+    The table below shows the names, colour, and favourite foods of three cats.
+    Define an object for each cat. (For experienced programmers: we haven’t
+    covered classes yet.)
+
+
+    Name            Colour             Food
+    Oswald          Black              Milk
+    Henderson       Ginger             Chips
+    Quentin         Tabby and white    Curry
+   */
 
 }
+
+object test extends App {
+  object Oswald {
+    val colour: String = "Black"
+    val food: String = "Milk"
+  }
+  object Henderson {
+    val colour: String = "Ginger"
+    val food: String = "Chips"
+  }
+  object Quentin {
+    val colour: String = "Tabby and white"
+    val food: String = "Curry"
+  }
+
+  println(Oswald)
+}
+
+
