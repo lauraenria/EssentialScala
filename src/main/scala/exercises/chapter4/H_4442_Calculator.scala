06dd1919ed78220e62d0ea16c8c4bff35d1d6b8b
Laura Enria <enria.laura2@gmail.com>
2020-09-14 13:48:59 +0100

    add: 4442 Calculator

--- src/main/scala/exercises/chapter4/H_4442_Calculator.scala
+++ src/main/scala/exercises/chapter4/H_4442_Calculator.scala
@@ -7,5 +7,13 @@ class H_4442_Calculator {
     A calculation may succeed (with an Int result)
     or fail (with a String message).
     Implement this.
+
+    Why did they choose to use a sealed keyword in this exercise?
+    Why sealed trait exists?
   */
 }
+
+
+sealed trait Calculation
+final case class Success(result: Int) extends Calculation
+final case class Failure(reason: String) extends Calculation
