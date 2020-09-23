3bd6799da54655a8dfaafbf6e167cc9635f8601c
Laura Enria <enria.laura2@gmail.com>
2020-09-23 09:31:27 +0100

    Chapter5 add: 5441 Maybe that Was a Mistake

---
+++ src/main/scala/exercises/chapter5/H_5441_Exercise_Maybe_that_Was_a_Mistake.scala
@@ -0,0 +1,20 @@
+package exercises.chapter5
+
+class H_5441_Exercise_Maybe_that_Was_a_Mistake {
+  /*
+    Create a generic trait called Maybe of a generic type A with two subtypes,
+    Full containing an A , and Empty containing no value.
+ */
+}
+
+trait Maybe[A]
+
+final case class Full[A](value: A) extends Maybe[A]
+final case class Empty[A]() extends Maybe[A]
+
+object H_5441_Exercise_Maybe_that_Was_a_Mistake_Test {
+
+  val perhaps1: Maybe[Int] = Empty[Int]
+  val perhaps2: Maybe[Int] = Full(1)
+
+}
