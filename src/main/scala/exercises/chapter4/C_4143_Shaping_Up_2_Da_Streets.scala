93cf4e4ef40a627cddda9e50f9146395b304de8f
Laura Enria <enria.laura2@gmail.com>
2021-01-04 15:41:23 +0000

    Chapter4 modify: 4143 4221

--- src/main/scala/exercises/chapter4/C_4143_Shaping_Up_2_Da_Streets.scala
+++ src/main/scala/exercises/chapter4/C_4143_Shaping_Up_2_Da_Streets.scala
@@ -15,30 +15,32 @@ class C_4143_Shaping_Up_2_Da_Streets {
 }
 /*
 // trait Shape ...
-  trait Shape {
-    def sides: Int
-    def perimeter: Double
-    def area: Double
-  }
+trait Shape {
+  def sides: Int
+  def perimeter: Double
+  def area: Double
+}
 // case class Circle ...
-  case class Circle(radius: Double) extends Shape {
-    val sides = 1
-    val perimeter: Double = 2 * math.Pi * radius
-    val area: Double = math.Pi * radius * radius
-  }
+case class Circle(radius: Double) extends Shape {
+  val sides = 1
+  val perimeter: Double = 2 * math.Pi * radius
+  val area: Double = math.Pi * radius * radius
+}
 
-  sealed trait Rectangular extends Shape {
-    def width: Double
-    def height: Double
-    val sides = 4
-    override val perimeter: Double = 2 * width + 2 * height
-    override val area: Double = width * height
-  }
-  case class Square(size: Double) extends Rectangular {
-    val width: Double = size
-    val height: Double = size
-  }
-  case class Rectangle(val width: Double, val height: Double)
-      extends Rectangular
+sealed trait Rectangular extends Shape {
+  def width: Double
+  def height: Double
+  val sides = 4
+  override val perimeter: Double = 2 * width + 2 * height
+  override val area: Double = width * height
+}
+case class Square(size: Double) extends Rectangular {
+  val width: Double = size
+  val height: Double = size
+}
+case class Rectangle(val width: Double, val height: Double) extends Rectangular
+
+object Shape {
+  val square: Square = Square(22)
 }
  */
--- src/main/scala/exercises/chapter4/D_4221_Printing_Shapes.scala
+++ src/main/scala/exercises/chapter4/D_4221_Printing_Shapes.scala
@@ -15,13 +15,45 @@ class D_4221_Printing_Shapes {
     // res2: String = A rectangle of width 3.0cm and height 4.0cm
  */
 }
-/*
+
+// trait Shape ...
+sealed trait Shape {
+  def sides: Int
+  def perimeter: Double
+  def area: Double
+}
+// case class Circle ...
+case class Circle(radius: Double) extends Shape {
+  val sides = 1
+  val perimeter: Double = 2 * math.Pi * radius
+  val area: Double = math.Pi * radius * radius
+}
+
+sealed trait Rectangular extends Shape {
+  def width: Double
+  def height: Double
+  val sides = 4
+  override val perimeter: Double = 2 * width + 2 * height
+  override val area: Double = width * height
+}
+case class Square(size: Double) extends Rectangular {
+  val width: Double = size
+  val height: Double = size
+}
+case class Rectangle(val width: Double, val height: Double) extends Rectangular
+
 object Draw extends App {
+  val square: Square = Square(22)
   def apply(shape: Shape): String = shape match {
+    case Square(size) => s"A square of size $size cm"
     case Rectangle(width, height) =>
       s"A rectangle of width ${width}cm and height ${height}cm"
-    case Square(size)   => s"A square of size ${size}cm"
     case Circle(radius) => s"A circle of radius ${radius}cm"
   }
+  println(Draw(square))
+
+  Draw(Circle(10))
+  // res1: String = A circle of radius 10.0cm
+  Draw(square)
+  // res2: String = A square of size 22.0cm
 }
- */
